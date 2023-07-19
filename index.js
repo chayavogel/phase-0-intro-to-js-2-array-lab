8c7084ed956d1dbf6e6a4bb235f8257b9af925d0
chaya <chayavogel1@gmail.com>
2023-07-19 16:22:47 -0400

    complete lab in phase 1

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
-  function destructivelyAppendCat(name) {
-    return cats.push(name);
-  }
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
 
-  function destructivelyPrependCat(name) {
-    return cats.unshift(name);
-  }
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
 
-  function  destructivelyRemoveLastCat() {
-    return cats.pop();
-  }
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
 
-  function destructivelyRemoveFirstCat() {
-    return cats.shift();
-  }
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
 
-  function appendCat(name) {
-    return [...cats, name];
-  }
+function appendCat(name) {
+  return [...cats, name]
+}
 
-  function prependCat() {
-    return ["Arnold", ...cats];
-  }
+function prependCat(name) {
+  return [name, ...cats]
+}
 
+function removeLastCat(name) {
+  return [...cats.slice(0,2)]
+}
 
-  function removeLastCat() {
-    return cats.slice(0, (cats.length-1));
-  }
-
-  function removeFirstCat() {
-    return cats.slice(1, (cats.length));
-  }
+function removeFirstCat(name) {
+  return [...cats.slice(1)]
+}
